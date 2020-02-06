afa95a4d9b0bbb74da1c15acf9eb944152ae6299
Caleb Carithers <calebcarithers@Calebs-MBP.home>
2020-02-06 10:44:41 -0500

    update / need to remove models but im lazy

--- server.js
+++ server.js
@@ -11,6 +11,30 @@ app.get('/noseDraw', (req, res) => {
 app.get('/p5ML5', (req, res) => {
     res.sendFile(path.join(__dirname, './p5ML5.html'));
 })
+app.get('/eyeFollower', (req, res) => {
+    res.sendFile(path.join(__dirname, './eyeFollower.html'));
+})
+app.get('/highFive', (req, res) => {
+    res.sendFile(path.join(__dirname, './highFive.html'));
+})
+app.get('/8thwall', (req, res) => {
+    res.sendFile(path.join(__dirname, './8thwall-ml5.html'));
+})
+app.get('/controlRig', (req, res) => {
+    res.sendFile(path.join(__dirname, './controlRig.html'));
+})
+app.get('/p5', (req, res) => {
+    res.sendFile(path.join(__dirname, './p5fun.html'));
+})
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, './test.html'));
+})
+
+app.get('/newtest', (req, res) => {
+    res.sendFile(path.join(__dirname, './newtest.html'));
+})
+
 
 app.use('/modules', express.static(__dirname + '/node_modules'));
 app.use('/static', express.static(__dirname + '/static'));
